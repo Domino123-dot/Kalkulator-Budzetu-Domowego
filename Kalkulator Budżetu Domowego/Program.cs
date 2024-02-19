244595ddc4141e5ef1469e4dfbf31a644ee35982
Kot <Kot@DESKTOP-0FCKOB5>
2024-02-19 09:40:55 +0100

    Dodaj pliki projektów.

---
+++ Kalkulator Budżetu Domowego/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+ static void Menu()
+{
+    Console.WriteLine("╔════════════════════════════════════════════════════════════════════════════════════════╗");
+    Console.WriteLine("                                             MENU                                         ");
+    Console.WriteLine("╚════════════════════════════════════════════════════════════════════════════════════════╝");
+
+
+}
+
+
+Menu();
+Console.WriteLine("1.Obliczanie Budżetu domowego");
+Console.WriteLine("2.Wyniki ostatniego obliczania");
+Console.WriteLine("3.Instrukcje pojęć");
